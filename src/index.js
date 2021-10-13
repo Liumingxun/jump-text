aeaf1b493db95a1be3c0dad2883420c680f43c82
liumingxun <18945504808@163.com>
2021-10-13 19:24:02 +0800

    重写

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import charming from "charming";
 import './public.css'
 
 export default function jump_text(elem, opt = {}) {
-    charming(elem)
-    let letters = elem.querySelectorAll('span')
-    for (let i = 0; i < letters.length; i++) {
-        let span = letters[i]
-        span.className = 'jump'
-        span.style.animationDelay = `${Math.log(i) * 500}ms`
-    }
+  console.log(elem.textContent)
 }
